e8818e9834c150e379debcfefd111af07261e0fd
Rotholtz <danielle.rotholtz@gmail.com>
2021-11-19 10:32:48 +0200

    initial view, logos and admin flow

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,120 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+
+
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar style="width:125px; height:88px">
+            <img src="~assets/180white.webp" alt = "logo" >
+          </q-avatar>
+        </q-toolbar-title>
+
+        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+      </q-toolbar>
+    </q-header>
+
+
+<!--      show-if-above
+      bordered
+      :width="200"
+      :breakpoint="500"
+      class="bg-grey-3"-->
+
+    <q-drawer v-model="rightDrawerOpen" style="direction:rtl" side="right"  behavior="desktop" elevated>
+      <q-scroll-area class="fit">
+        <q-list padding class="menu-list">
+          <q-item 
+          to = "/groups"
+          clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="groups" />
+            </q-item-section>
+
+            <q-item-section class = "drawer-items">
+              קבוצות
+            </q-item-section>
+          </q-item>
+
+          <q-item 
+          to="/trainers"
+          clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="sports" />
+            </q-item-section>
+
+            <q-item-section class = "drawer-items">
+              מאמנים
+            </q-item-section>
+          </q-item>
+
+          <q-item 
+          to = "/volunteers"
+          clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="emoji_people" />
+            </q-item-section>
+
+            <q-item-section class = "drawer-items">
+              מתנדבים
+            </q-item-section>
+          </q-item>
+
+          <q-item 
+          to = "/trainees"
+          clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="sentiment_very_satisfied" />
+            </q-item-section>
+
+            <q-item-section class = "drawer-items">
+              מתאמנים
+            </q-item-section>
+          </q-item>
+
+          <q-item 
+          to = "/"
+          clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="logout" />
+            </q-item-section>
+
+            <q-item-section class = "drawer-items">
+              יציאה
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const rightDrawerOpen = ref(false)
+
+    return {
+      rightDrawerOpen,
+      toggleRightDrawer () {
+        rightDrawerOpen.value = !rightDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.drawer-items{
+  font-family: "lucida grande", tahoma, verdana, arial, sans-serif; 
+  font-weight: bold;
+  color: #1D2172;
+}
+</style>
---
+++ src/pages/admin_groups.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="q-pa-md float-right" style="max-width: 350px">
+    <h3 class="group_header  q-mb-none">קבוצות</h3>
+    <q-list bordered separator style="direction:rtl" class="list-items">
+      <q-item to="/groups/id" clickable v-ripple>
+        <q-item-section>קבוצה 1 - תל אביב</q-item-section>
+      </q-item>
+
+      <q-item to="/groups/id" clickable v-ripple>
+        <q-item-section>
+          <q-item-label>קבוצה 2 - רמת גן</q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-item to="/groups/id" clickable v-ripple>
+        <q-item-section>
+          <q-item-label>קבוצה 3 - הרצליה</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+
+
+    <!-- <q-list bordered class="rounded-borders reverse row  ">
+      <q-expansion-item
+        expand-separator
+        label="קבוצה 1"
+      >
+        <q-card>
+          <q-card-section>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+            commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+            eveniet doloribus ullam aliquid.
+          </q-card-section>
+        </q-card>
+      </q-expansion-item>
+
+      <q-expansion-item
+        expand-separator
+        label="קבוצה 2"
+      >
+        <q-card>
+          <q-card-section>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+            commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+            eveniet doloribus ullam aliquid.
+          </q-card-section>
+        </q-card>
+      </q-expansion-item>
+
+      <q-expansion-item
+        expand-separator
+        icon="drafts"
+        label="קבוצה 3"
+      >
+        <q-card>
+          <q-card-section>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+            commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+            eveniet doloribus ullam aliquid.
+          </q-card-section>
+        </q-card>
+      </q-expansion-item>
+
+    </q-list> -->
+  </div>
+</template>
+<style>
+.q-table th {
+  text-align:right;
+  font-family: "lucida grande", tahoma, verdana, arial, sans-serif; 
+  font-weight: bold;
+  color: #1D2172;
+}
+
+.q-table td {
+  text-align:right;
+  font-family: "lucida grande", tahoma, verdana, arial, sans-serif; 
+  
+  color: #1D2172;}
+.group_header {
+  text-align:right;
+    font-weight: bold;
+  font-family: "lucida grande", tahoma, verdana, arial, sans-serif; 
+
+  color: #1D2172;
+}
+
+.list-items{
+  font-family: "lucida grande", tahoma, verdana, arial, sans-serif; 
+  font-weight: bold;
+  font-size: large;
+  color: #1D2172;
+}
+</style>
+
