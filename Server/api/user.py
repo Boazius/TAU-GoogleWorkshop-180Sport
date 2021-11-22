5c002af7e127f7534efd26a564c38906c7fdd31d
dana-troushinsky <dana.troushinsky@siteaware.com>
2021-11-22 09:40:31 +0200

    added server side directory

--- Server/api/user.py
+++ Server/api/user.py
@@ -1,5 +1,4 @@
 import datetime
-
 import flask
 import jwt
 from flask import Blueprint, jsonify
@@ -16,28 +15,16 @@ def login():
     from Server.main import db, app
     auth = flask.request.authorization
     if not auth or not auth.username or not auth.password:
-        return jsonify({"message": "Authentication credentials were not supplied"}), 400
-    user = db.session.query(User).filter_by(full_name=auth.username).first()
-    if not user:
-        return jsonify({"message": "User does not exist"}), 400
-    if check_password_hash(user.password, auth.password):
+        return jsonify({"success": False, "message": "Authentication credentials were not supplied"}), 400
+    user_from_db = db.session.query(User).filter_by(email=auth.username).first()
+    if not user_from_db:
+        return jsonify({"success": False, "message": "User does not exist with current email"}), 400
+    if check_password_hash(user_from_db.password, auth.password):
         token = jwt.encode(
-            {'id': user.id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},
-            app.config['SECRET_KEY'], algorithm="HS256")
-        # return jsonify({'token': jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])})
-        return jsonify({'token': token})
-    return jsonify({"message": "Incorrect password"}), 400
-
-    # json_data = flask.request.json
-    # email = json_data['email']
-    # password = json_data['password']
-    # remember = json_data['remember']
-    # from main import db
-    # user = db.session.query(User).filter_by(email=email).first()
-    # if not user:
-    # return str("User does not exist")
-    # login_user(user, remember=remember)
-    # return jsonify(user.to_dict())
+            {'id': user_from_db.id, 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30), 'email':
+                user_from_db.email}, app.config['SECRET_KEY'], algorithm="HS256")
+        return jsonify({'success': True, 'token': token, 'user': user_from_db.to_dict()})
+    return jsonify({'success': False, "message": "Incorrect password"}), 400
 
 
 @user.post('/signup')
@@ -47,7 +34,7 @@ def signup():
         data = flask.request.json
         user_exists = db.session.query(User).filter_by(email=data['email']).first()
         if user_exists:
-            return jsonify({'success':False, 'message': 'User with current email is already exists'})
+            return jsonify({'success': False, 'message': 'User with current email is already exists'}), 400
         hashed_password = generate_password_hash(data['password'], method='sha256')
         new_user = User(user_type=int(data['user_type']), email=data['email'], full_name=data['full_name'],
                         phone_number=str(data['phone_number']), active_or_not=True, attendance=int(1),
@@ -56,16 +43,44 @@ def signup():
         db.session.commit()
         return jsonify({"success": True, "user": new_user.to_dict()})
     except:
-        return jsonify({"success": False, "message": "Something went wrong"})
+        return jsonify({"success": False, "message": "Something went wrong"}), 400
 
 
 @user.post('/logout')
 def logout():
+    # !!!!!!!THINK ABOUT WHAT DOING HERE!!!!! #####
     return 1
 
 
-@user.put('/user')
-def update_user():
+@user.put('/user/<user_id>')
+@token_required
+def update_user_by_id(current_user, user_id):
+    from Server.main import db
+    if current_user.user_type in [3, 4] and current_user.id != user_id:
+        return jsonify({"success": False,
+                        "message": "User cannot update different user details, unless it is admin/trainer"}), 401
+    data = flask.request.json
+    user_from_db = db.session.query(User).filter_by(id=user_id).first()
+
+    for key in data.keys():
+        if key == 'email':
+            user_exists = db.session.query(User).filter_by(email=data['email']).first()
+            if user_exists:
+                return jsonify({'success': False, 'message': 'User with current email is already exists'}), 400
+            user_from_db.email = data['email']
+        if key == 'password':
+            hashed_password = generate_password_hash(data['password'], method='sha256')
+            user_from_db.password = hashed_password
+        if key == 'full_name':
+            user_from_db.full_name = data['full_name']
+        if key == 'phone_number':
+            user_from_db.phone_number = str(data['phone_number'])
+        if key == 'attendance':
+            user_from_db.attendance = data['attendance']
+    db.session.commit()
+    return jsonify({"success": True, "user": user_from_db.to_dict()})
+    # except:
+      # return jsonify({"success": False, "message": "Something went wrong"}), 400
     return 1
 
 
@@ -77,11 +92,11 @@ def delete_user():
 @user.get('/user/<user_id>')
 @token_required
 def get_user(current_user, user_id):
-    if current_user.user_type == 3 or current_user.user_type == 4:
-        if current_user.id != user_id:
-            return jsonify({'message': 'Unauthorized'}), 401
-    from main import db
-    user = db.session.query(User).filter_by(id=user_id).first()
-    if not user:
-        return jsonify({'message': 'No user found!'})
-    return jsonify({'user': user.to_dict()})
+    from Server.main import db
+    if current_user.user_type in [3, 4] and current_user.id != user_id:
+        return jsonify({"success": False,
+                        "message": "User cannot view different user details, unless it is admin/trainer"}), 401
+    user_from_db = db.session.query(User).filter_by(id=user_id).first()
+    if not user_from_db:
+        return jsonify({'success': False, 'message': 'No user found!'})
+    return jsonify({'success': True, 'user': user_from_db.to_dict()})
