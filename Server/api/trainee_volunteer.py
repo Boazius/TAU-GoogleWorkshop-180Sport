6eb2def3dcb5af1be9e0451bca3afe517508568f
GalEmanuel <galemanuel@mail.tau.ac.il>
2021-12-12 13:55:34 +0200

    update url at trainee,add to dict at models training, update function get all user admin, change training

--- Server/api/trainee_volunteer.py
+++ Server/api/trainee_volunteer.py
@@ -7,7 +7,7 @@ from utils import token_required, login_required
 trainee = Blueprint('trainee_volunteer', __name__)
 
 
-@trainee.post('/trainee/<user_id>/<training_id>/message')
+@trainee.post('/trainee/message/<user_id>/<training_id>/')
 @token_required
 def post_message(current_user,user_id,training_id):
     from main import db
@@ -32,33 +32,8 @@ def post_message(current_user,user_id,training_id):
     except:
         return jsonify({"success": False, "message": "Something went wrong"}), 400
 
-""""
-כפילות - לא צריך
-@trainee.put('/trainee/<user_id>/<training_id>/message')
-@token_required
-def put_message(current_user,user_id,training_id):
-    from Server.main import db
-    if current_user.user_id != user_id:
-        return jsonify({"success": False,
-                        "message": "User cannot update message, unless it is the fit user"}), 401
-    training_from_db = db.session.query(Group).filter_by(id=training_id).first()
-    if not training_from_db:
-        return jsonify({'success': False, 'message': 'No training found!'})
-    try:
-        data = flask.request.json
-        message = data['message']
-        notes = json.loads(training_from_db.notes)
-        notes['user_id'] = message
-        db.session.commit()
-        return jsonify({"success": True,
-                        "message": "message: " + message + "from user: " + user_id + "add to training successfully"})
-    except:
-        return jsonify(
-            {"success": False, "message": "Something went wrong"}), 400
-"""
 
-
-@trainee.delete('/trainee/<user_id>/<training_id>/message')
+@trainee.delete('/trainee/message/<user_id>/<training_id>/')
 @token_required
 def delete_message(current_user,user_id,training_id):
     from main import db
@@ -87,9 +62,9 @@ def get_closest_training_by_group_id(current_user,user_id,group_id):
 """""
 
 
-@trainee.get('/trainee/<user_id>/get_closest_training_by_user_id/<training_id>/')
+@trainee.get('/trainee/get_closest_training_by_user_id/<user_id>/<training_id>/')
 @token_required
-def get_closest_training_by_group_id(current_user, user_id, training_id):
+def get_closest_training_by_user_id(current_user, user_id, training_id):
     from main import db
     training_from_db = db.session.query(Training).filter_by(id=training_id).first()
     if current_user.user_type in [3,4] and current_user.user_id != user_id:
@@ -102,7 +77,7 @@ def get_closest_training_by_group_id(current_user, user_id, training_id):
                     "message": training_from_db.to_dict()}), 401
 
 
-@trainee.put('/trainee/<user_id>/update_attendance')
+@trainee.put('/trainee/update_attendance/<user_id>/')
 @token_required
 def update_attendance(current_user, user_id):
     from main import db
