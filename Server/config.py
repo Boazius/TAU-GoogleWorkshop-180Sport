00cacc4f8c21c5e7d03b7289abb4b67aad9620c6
dana-troushinsky <dana.troushinsky@siteaware.com>
2022-02-04 22:12:45 +0200

    Add DB

--- Server/config.py
+++ Server/config.py
@@ -7,7 +7,7 @@ from datetime import timedelta
 
 class BaseConfig(object):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///180.db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:G6kdi6pN0AFxo01x@35.240.11.106/db180'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     # used for encryption and session management
     SECRET_KEY = '111mysecretkey111'
