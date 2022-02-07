6fd987d439bf92d973ca225cf0a35f241b083f21
dana-troushinsky <dana.troushinsky@siteaware.com>
2022-02-07 15:45:25 +0200

    automatic creation of trainings

--- Server/api/training.py
+++ Server/api/training.py
@@ -112,6 +112,8 @@ def post_training_by_group_id(current_user):
             group_from_db.trainings_list = listToString(training_list)
             db.session.commit()
             return jsonify({"success": True, "training": training_from_db.to_dict()})
+        else:
+            return jsonify({"success": True, "training": "no training has created"}), 200
     except:
         return jsonify({"success": False, "message": "Something went wrong"}), 400
 
