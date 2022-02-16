5c0428dc7fb863f8bf4f721b79f9ec2b19041e8e
dana-troushinsky <dana.troushinsky@siteaware.com>
2022-02-16 13:33:52 +0200

    automatic creation of trainings

--- Server/add_new_user.py
+++
@@ -1,7 +0,0 @@
-from main import db
-from models import User, User_type, Training, Group
-from datetime import datetime, date
-from flask import Blueprint, jsonify
-
-current_user = db.session.query(User).filter_by(email="dana").first()
-print(current_user)
